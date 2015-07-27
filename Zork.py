07504f7ec41bafcf0acb18da5375ba19e0cb422a
LeonardoGuandalini <leoguanda@gmail.com>
2015-07-27 17:50:44 -0300

    Updated Zork - PROBLEM: UseItem function won't work

--- Zork.py
+++ Zork.py
@@ -1,6 +1,6 @@
 
-
 def intro():
+    HP = 100
     print("Player, in the sections RH and LH, the numbers 0 - 4 are the amount of damage your weapon does. Nothing = 0; Dagger = 1; Sword = 2; Longsword = 3; Divine sword = 4; Double dagger = 3.")
     nothing = 0
     dagger = 1
@@ -8,6 +8,8 @@ def intro():
     longsword = 3
     divine_sword = 4
     double_dagger = 3
+    small_potion = HP + 10
+    map1 = "It shows the map of the village and a castle towards it."
 
     one = nothing
     two = dagger
@@ -15,6 +17,8 @@ def intro():
     four = longsword
     five = divine_sword
     six = double_dagger
+    seven = small_potion
+    eight = map1
 
 
 
@@ -23,10 +27,12 @@ def intro():
     Coins = 0
     RH = one
     LH = one
+    Inventory = {"small_potion" : 0, "maps" : 0}
     #START OF COMMENT - SHOWS HOW MANY HP AND COINS YOU HAVE
     def Status():
         print "You have %s HP and %s Coins!" % (HP, Coins)
         print "Items currently equiped: RH = %s & LH = %s" % (RH, LH)
+        print "Items in inventory: %s. " % (Inventory)
     print "Do you wanna see your status?"
     answer = raw_input("Y or N? ")
     if answer == "Y":
@@ -34,6 +40,32 @@ def intro():
     else:
         print "Ok"
     #END OF COMMENT
+    #Start of new function UseItem
+    def UseItem():
+        print Inventory
+        Choice = raw_input("Which item do you want to use?: ")
+        if Choice == "small_potion":
+            if Inventory["small_potion"] > 0:
+                if HP <= 90:
+                    HP += 10
+                    Inventory["small_potion"] -= 1
+                    print Inventory
+                    Status()
+    #END OF FUNCTION
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     print("West Side")
     print("You're in a house and there's a path to your left")
     print("1. Follow Path")
@@ -54,7 +86,7 @@ def intro():
                 print ("You arrived the village at 10:56 pm and it was raining. There's a (hotel?) to your left, a store towards you and a tavern by your right.")
                 print ("Where would you like to go?: ")
                 answer4 = input("1. Hotel; 2.Store; 3.Tavern")
-                if answer4 == 1:
+                if answer4 == 1:#Hotel
                     print("There was a woman inside. She says: Would you like to rest? 10 coins.")
                     answer_Hotel1 = raw_input("Would you like to rest? (Recovers 10HP): ")
                     if answer_Hotel1 == "Yes":
@@ -71,15 +103,40 @@ def intro():
                     else:
                         print("Ok then.")
 
-                elif answer4 == 2:
+                elif answer4 == 2: #Store
                     print("There was a man inside. He says: Hay mate. Would you like to buy some gear?")
+                    answer_Shop1 = input("1.Yes, 2.No: ")
+                    items = ["Dagger, Small Potion, Map"]
+                    if answer_Shop1 == 1:
+                        print items
+                        print("What would you like to buy?")
+                        buyChoice = input("1. Dagger, 2.Potion, 3.Map: ")
+                        if buyChoice == 1:
+                            Shop1 = raw_input("So you wanna buy a Dagger. 3 coins.")
+                            if Shop1 == "Yes":
+                                if Coins >= 3:
+                                    handChoice = raw_input("You bought it. In which hand do you want to equip it?: ")
+                                    if handChoice == "RH":
+                                        RH = two
+                                    else:
+                                        LF = two
+                                        Status()
+                        elif buyChoice == 2:
+                            Shop2 = raw_input("2 coins: ")
+                            if Shop2 == "Yes":
+                                if Coins >= 2:
+                                    Inventory["small_potion"] = 1
+                                    Status()
+                        elif buyChoice == 3:
+                            print("wait")
 
 
 
 
 
-                elif answer4 == 3:
-                    print("It was crowded. There was a paper hanging on the wall that said that the beer was 2 coins. Drink one?")
+
+                    elif answer4 == 3:#Tavern
+                        print("It was crowded. There was a paper hanging on the wall that said that the beer was 2 coins. Drink one?")
 
 
 
@@ -91,7 +148,7 @@ def intro():
             print ("You arrived the village at 10:56 pm and it was raining. There's a (hotel?) to your left, a store towards you and a tavern by your right.")
             print ("Where would you like to go?: ")
             answer4 = input("1. Hotel; 2.Store; 3.Tavern")
-            if answer4 == 1:
+            if answer4 == 1: #Hotel
                 print("There was a woman inside. She says: Would you like to rest? 10 coins.")
                 answer_Hotel1 = raw_input("Would you like to rest? (Recovers 10HP): ")
                 if answer_Hotel1 == "Yes":
@@ -108,9 +165,35 @@ def intro():
                 else:
                     print("Ok then.")
 
-            elif answer4 == 2:
+            elif answer4 == 2:#Store
                 print("There was a man inside. He says: Hay mate. Would you like to buy some gear?")
+                answer_Shop1 = input("1.Yes, 2.No: ")
+                items = ["Dagger, Small Potion, Map"]
+                if answer_Shop1 == 1:
+                    print items
+                    print("What would you like to buy?")
+                    buyChoice = input("1. Dagger, 2.Potion, 3.Map: ")
+                    if buyChoice == 1:
+                        Shop1 = raw_input("So you wanna buy a Dagger. 3 coins.")
+                        if Shop1 == "Yes":
+                            if Coins >= 3:
+                                handChoice = raw_input("You bought it. In which hand do you want to equip it?: ")
+                                if handChoice == "RH":
+                                    RH = two
+                                else:
+                                    LF = two
+                    elif buyChoice == 2:
+                        Shop2 = raw_input("2 coins")
+                        if Shop2 == "Yes":
+                            if Coins >= 2:
+                                Inventory["small_potion"] = 1
+                                Coins -= 2
+                                Status()
+                                UseItem()
 
+
+                    elif buyChoice == 3:
+                        print("wait")
 
 
 
