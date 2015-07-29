436d352a7a30a6b3b474550e691014f9a31670f8
LeonardoGuandalini <leoguanda@gmail.com>
2015-07-28 21:58:37 -0300

    Updated ST1.py -- Which is basically the main story

--- ST1.py
+++ ST1.py
@@ -4,7 +4,7 @@ class Players:
 		self.maxhp = maxhp
 		self.attack = attack
 		self.battlehp = battlehp
-
+		
 
 
 class Warrior:
@@ -20,15 +20,16 @@ class WildBeast:
 		self.attack = attack
 
 
+
 ########################## - Classes
 
-########################## - Alive stuff
+########################## - 'Alive stuff'
 Player = Players(10, 10, 2, 0)
 Skeleton = Warrior(5, 1)
 Dog = WildBeast(3, 1)
 Wolf = WildBeast(6, 2)
+GiantSpider = WildBeast(20, 2)
 ########################## - Alive stuff
-
 ########################## - Functions
 def lvlup():
 	if Dog.hp or Wolf.hp or Skeleton.hp < 0:
@@ -36,8 +37,17 @@ def lvlup():
 		Player.attack += 1
 
 
+def usePotion():
+	if Player.battlehp != Player.maxhp:
+		playerInventory['potion'] -= 1
+		print playerInventory
+		Player.battlehp = Player.maxhp
 
 
+
+
+
+playerInventory = {'potion': 1}
 
 
 print("Welcome to Zork, a text based game")
@@ -50,14 +60,57 @@ if Choice1 == 1:
 		Player.battlehp -= Dog.attack
 		print Dog.hp
 	lvlup()
+	print("MAXHP, CURRENT HP, ATTACK, COINS")
 	print(Player.maxhp, Player.battlehp, Player.attack, Player.coins)
 	print('Done, you defeated the beast and walked North.')
+	print("After that, you entered a cave, where there was an old man. You couldn't see him very, because of the fog. ")
+	Choice2 = input("Your options are: 1.Explore cave and use potion ; 2.Talk to old  ")
+	if Choice2 == 1:
+		usePotion()
+		chestChoice1 = input("Open chest?: 1.Yes 2.No")
+		if chestChoice1 == 1:
+			Player.attack += 3
+			print('You found a sword! +3 attack; You went back to the cave and  you talked to the old man, and he started to transform into a giant spider, and attacked you!')
+			while GiantSpider.hp >= 0 and Player.battlehp >= 0:
+				GiantSpider.hp -= Player.attack
+				Player.battlehp -= GiantSpider.attack
+				if Player.battlehp <= 0:
+					print("Game Over -- You might consider using that potion you got next time!")
+				print GiantSpider.hp
+				print Player.battlehp	
+
+
+		else:
+			print('You went back to the cave, where the old man is. You talked to him, and he started to transform into a giant spider, and attacked you!')
+
+	elif Choice2 == 2:
+		print("You talked to the old man, and he started to transform into a giant spider, and attacked you!'")
+#################################################################################################### ~ Other path		
 elif Choice1 == 2:
 	Player.coins += 20
 	print("This is worth 20 coins!")
 	print("You walked into the forest where you found a statue, where you could read: 'Someone died here a long time ago and his wealth was barried somewhere around here'.")
 	print("By the moment had finished reading, a wolf jumped and atacked you!")
+	while Dog.hp > 0:
+		Dog.hp -= Player.attack
+		Player.battlehp -= Dog.attack
+		print Dog.hp
+	lvlup()
+	print("MAXHP, CURRENT HP, ATTACK, COINS")
+	print(Player.maxhp, Player.battlehp, Player.attack, Player.coins)
+	print('Done, you defeated the beast and walked North.')
+	print("After that, you entered a cave, where there was an old man. You couldn't see him very, because of the fog. ")
+	Choice2 = input("Your options are: 1.Explore cave; 2.Talk to old ")
+	if Choice2 == 1:
+		chestChoice1 = input("Open chest?: 1.Yes 2.No")
+		if chestChoice1 == 1:
+			Player.attack += 3
+			print('You found a sword! +3 attack; You went back to the cave and  you talked to the old man, and he started to transform into a giant spider, and attacked you!')
+					
 
+
+		else:
+			print('You went back to the cave, where the old man is. You talked to him, and he started to transform into a giant spider, and attacked you!')
 
 
 
